490912bbe897c317d0cf97a0b2e06a68bd9b7617
Joao Guerreiro <joao.guerreiro@ctw.bmwgroup.com>
2021-11-27 17:53:49 +0000

    add Route plan

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 
 ///Controller imports
-const UserController = require('./controllers/user_controller')
+const UserController = require('./controllers/user_controller');
+const PlanController = require('./controllers/plans_controller')
 
 router.post('/login', UserController.login)
 router.post('/signup', UserController.signup)
+router.post('/plan', PlanController.getPlanFromId)
 
 
 module.exports = router
